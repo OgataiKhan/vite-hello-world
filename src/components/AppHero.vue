1428fadd59b42320408d656cf1b3f681277b1a7d
OgataiKhan <badellino.kevin@gmail.com>
2023-12-06 16:07:53 +0100

    feat: add jumbotron & title

---
+++ src/components/AppHero.vue
@@ -0,0 +1,38 @@
+<script>
+import TitleComponent from '../components/jumbotron/AppTitle.vue';
+
+export default {
+  name: 'JumbotronComponent',
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    TitleComponent,
+  },
+};
+
+</script>
+
+<template>
+  <div class="container">
+    <TitleComponent />
+    <p>This is a jumbotron. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate accusamus saepe dolores enim nam, blanditiis asperiores facilis quibusdam dolorem ea ex delectus illum voluptas deserunt! Explicabo repudiandae officia impedit alias!</p>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    background-color: #7f5af0;
+    text-align: center;
+    padding-top: 100px;
+    padding-bottom: 100px;
+  }
+
+  .container p {
+    max-width: 1000px;
+    margin: 0 auto;
+    font-weight: 500;
+  }
+</style>
---
+++ src/components/jumbotron/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'TitleComponent',
+  data() {
+    return {
+
+    };
+  },
+};
+
+</script>
+
+<template>
+  <h1>My first app using Vite!</h1>
+</template>
+
+<style scoped>
+ h1 {
+  padding-bottom: 50px;
+ }
+</style>
